d20a95bb5cc1e277c54f23c030f0bb2783610c53
Yahday A <64969363+Yahday@users.noreply.github.com>
2021-04-16 19:49:58 -0500

    Contact Form

--- script.js
+++ script.js
@@ -1,37 +1,70 @@
 ;(function(){
-    function id(v){return document.getElementById(v); }
+    const id = (v)=> document.getElementById(v); 
     
-    function loadbar() {
+    const loadbar = ()=> {
         const ovrl = id("overlay"),
             prog = id("progress"),
             bar = id("progressbar")
 
         let img = document.images,
             c = 0,
+            stop = 0,
             tot = img.length;
         
-        function imgLoaded(){
+        const doneLoading= () => {
+            ovrl.style.opacity = 0;
+            setTimeout(()=>{ 
+                ovrl.style.display = "none";
+            }, 1400); 
+        }
+
+        const imgLoaded= ()=> {
+            clearInterval(timer)
             c += 1;
             var perc = ((100/tot*c) << 0) +"%";
             bar.style.width = perc;
             prog.innerHTML = "Loading "+ perc;
             if(c===tot) return doneLoading();
         }
-
-        function doneLoading(){
-            ovrl.style.opacity = 0;
-            setTimeout(function(){ 
-                ovrl.style.display = "none";
-            }, 1400); 
-        }
+        
+        var autoInc = ()=>{
+            console.log("hola")
+            stop++
+            var perc = (stop) +"%";
+            bar.style.width = perc;
+            prog.innerHTML = "Loading "+ perc;
+            if (stop == 50) {
+                clearInterval(timer)
+            }
+        }  
+        var timer = setInterval(autoInc, 250);
 
         for(var i=0; i<tot; i++) {
             var tImg     = new Image();
             tImg.onload  = imgLoaded;
             tImg.onerror = imgLoaded;
             tImg.src     = img[i].src;
-        }    
+        }     
+    }
+    document.addEventListener('DOMContentLoaded', loadbar, false);
+
+    const forWeb = id("web"),
+          forApp = id("app"),
+          toMail = id("toMail"),
+          toWhats = id("toWhats");
+    
+    const changeLink = ()=> {
+        let prod = ''
+        if (forApp.checked) prod = "una%20Aplicaci%C3%B3n%20Web"
+        if (forWeb.checked) prod = "un%20Sitio%20Web"
+        if (!forWeb.checked && !forApp.checked) prod = "uno%20de%20los%20productos"
+    
+        toMail.href = `mailto:yajday@hotmail.com?subject=Pedir%20m%C3%A1s%20informaci%C3%B3n&body=Buenas%20tardes%2C%20estoy%20interesado%20en%20${prod}.%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20por%20favor.`
+
+        toWhats.href = `https://wa.me/5214492201244?text=Buenas%20tardes%2C%20estoy%20interesado%20en%20${prod}.%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20por%20favor.`
     }
 
-    document.addEventListener('DOMContentLoaded', loadbar, false);
+    forWeb.addEventListener('change', changeLink)
+    forApp.addEventListener('change', changeLink)
+    
 }());
